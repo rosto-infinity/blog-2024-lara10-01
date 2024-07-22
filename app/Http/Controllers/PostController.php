355bd4fba7342fafcf8f03d96f6809ab15490d4a
rostodev <rostodev@gmail.com>
2024-07-22 03:53:50 +0100

    Les méthodes de scope (centraliser le filtre de recherche sur nos posts dans le contreller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,48 +13,29 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(request $request): view
+    public function index(Request $request): View
     {
-
-        $posts = Post::query();
-
-        if ($search = $request->search) {
-            $posts->where(
-                fn (Builder $query) => $query
-                    ->where('title', 'LIKE', '%' . $search . '%')
-                    ->orWhere('content', 'LIKE', '%' . $search . '%')
-            );
-        }
-
-        $posts = $posts->latest()->paginate(10);
-        return view('posts.index', compact('posts'));
+        return $this->postsView($request->search ? ['search' => $request->search] : []);
     }
 
     public function postsByCategory(Category $category): View
     {
-        return View('posts.index', [
-            // 'posts' => $category->posts()->latest()->paginate(7),
-
-            'posts' => Post::where(
-                'category_id',
-                $category->id
-            )->latest()->paginate(10),
-        ]);
+        return $this->postsView(['category' => $category]);
     }
 
     public function postsByTag(Tag $tag): View
     {
-        return View('posts.index', [
-            // 'posts' => $category->posts()->latest()->paginate(7),
+        return $this->postsView(['tag' => $tag]);
+    }
 
-            'posts' => Post::whereRelation(
-                'tags',
-                // 'id',
-                'tags.id',
-                $tag->id
-            )->latest()->paginate(5),
+
+    protected function postsView(array $filters): View
+    {
+        return view('posts.index', [
+            'posts' => Post::filters($filters)->latest()->paginate(10),
         ]);
     }
+
 
     public function show(Post $post): view
     {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -19,6 +21,30 @@ class Post extends Model
     {
         return 'slug';
     }
+
+
+   public function scopeFilters(Builder $query, array $filters): void
+{
+    if (isset($filters['search'])) {
+        $query->where(fn (Builder $query) => $query
+            ->where('title', 'LIKE', '%' . $filters['search'] . '%')
+            ->orWhere('content', 'LIKE', '%' . $filters['search'] . '%')
+        );
+    }
+
+    if (isset($filters['category'])) {
+        $query->where(
+            'category_id', $filters['category']->id ?? $filters['category']
+        );
+    }
+
+    if (isset($filters['tag'])) {
+        $query->whereHas('tags', function ($query) use ($filters) {
+            $query->where('tags.id', $filters['tag']->id ?? $filters['tag']);
+        });
+    }
+}
+
 
     public function category(): BelongsTo
     {
